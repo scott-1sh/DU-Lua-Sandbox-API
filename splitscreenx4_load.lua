07ae550a6c6a61e0b816f33eb5aad020c288bad0
nmare418 <dragonnerie@hotmail.com>
2020-06-09 20:26:55 -0400

    splitscreen - demo de splitter d'écran (2X2)

--- splitscreenx4_load.lua
+++ splitscreenx4_load.lua
@@ -1,38 +1,21 @@
---[[    
-    splitscreenx4_load.lua 
-    Copyright (C) 2020 Stephane Boivin (Devgeek studio enr.)
-    
-    This file is part of "DU offline sandbox API".
-
-    "DU offline sandbox API" is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    "DU offline sandbox API" is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with "DU offline sandbox API".  If not, see <https://www.gnu.org/licenses/>.
-]]
 
 showOnScreen(1)  
 verboseLua(1)
 verboseJava(0)
 
-svg = "\nlogo = [["..loadScript('./src/pictures/LogoDUAPI_SVG.svg').."]]\n"
+svg = "\nlogo = [["..loadScript('./src/pictures/logo.svg').."]]\n"
+-- svg = "\nlogo = [["..loadScript('./src/pictures/warehouse.svg').."]]\n"
+-- svg = "\nlogo = [["..loadScript('./src/pictures/warehouse2.svg').."]]\n"
+-- svg = "\nlogo = [["..loadScript('./src/pictures/LogoDUAPI_SVG.svg').."]]\n"
 
 -- Unit
-UnitStart = svg..loadScript('splitscreenx4_start.lua')
-
--- UnitTick = loadScript('ball_tick.lua')
+UnitStart =  svg..loadScript('splitscreenx4_start.lua')
+UnitTick = loadScript('splitscreenx4_tick.lua')
 UnitStop = "print('stopped')" 
 obj = Unit(UnitStart, UnitStop)
 
--- setup a timer named loop, using the UnitTick script
--- setupTimer(obj, 'loop', UnitTick)
+-- setup a timer named "loop" to use the UnitTick script
+setupTimer(obj, 'loop', UnitTick)
 
 obj = ScreenUnit('screen1', 306, 153)
 moveElement(obj, 200, 5)
--- splitscreenx4_start.lua
+++ splitscreenx4_start.lua
@@ -1,36 +1,71 @@
 
-screen1.activate()
-screen2.activate()
-screen2.clear()
--- 306, 153
-picture = '<svg width="306" height="153" viewBox="0 0 612 306">'..logo..'</svg>' 
---[[
-img = "<img src='data:image/svg+xml;utf8,%s' width=\"306\" height=\"153\">"
-viewbox2 = '<svg width="306" height="153" viewBox="0 0 153 76">'
--- viewbox3 = '<svg width="200vw" height="200vh" viewBox="0 50vw 100vw 100vh">'
-viewbox4 = '<svg  width="306" height="153" viewBox="0 0 100% 100%">'
-]]
--- <object type="image/svg+xml" data="kiwi.svg" class="logo">
---  Kiwi Logo <!-- fallback image in CSS -->
--- </object>
--- html
-svgBackground = [[ 
-  <rect width="100vw" height="100vh" style="fill:rgb(0,0,255);stroke-width:10;stroke:rgb(255,255,0)" />        
-  <text x="15vw" y="35vh" fill="white" font-family="Arial" font-size="15">%s</text>
-  ]]..logo 
+screenX = 306
+screenY = 153
+scale = 2
+ballRayon = 5
+speed = 1.2
+x = {17, 30, 100, 111, 70}  
+y = {17, 30, 100, 111, 70}
+dirx = {5, -4, 1, -2, 1}  
+diry = {5, 3, -3, 2, 3}
+
+SplitScreen4 = {screen1, screen2, screen3, screen4, screenX = 306, screenY = 153, scale = 2}
+function SplitScreen4:new(o)
+  o = o or {}  
+  setmetatable(o, self)
+  self.__index = self
+  
+  o.screen1 = screen1 
+  o.screen2 = screen2 
+  o.screen3 = screen3 
+  o.screen4 = screen4 
+-- img = "<img src='data:image/svg+xml;utf8,%s' width=\"306\" height=\"153\">"
+-- viewbox1 = [[<svg width="306" height="153" viewBox="0 0 306 153"><g transform="scale(2) translate(0 -100)"> 
+  o.viewbox1 = '<svg width="'..screenX..'" height="'..screenY..'" viewBox="0 0 '..screenX..' '..screenY..'"><g transform="scale('..scale..')">' 
+  o.viewbox2 = '<svg width="'..screenX..'" height="'..screenY..'" viewBox="'..screenX..' 0 '..screenX..' '..screenY..'"><g transform="scale('..scale..')">'
+  o.viewbox3 = '<svg width="'..screenX..'" height="'..screenY..'" viewBox="0 '..screenY..' '..screenX..' '..screenY..'"><g transform="scale('..scale..')">'
+  o.viewbox4 = '<svg width="'..screenX..'" height="'..screenY..'" viewBox="'..screenX..' '..screenY..' '..screenX..' '..screenY..'"><g transform="scale('..scale..')">'
+     
+  return o
+end
+
+function SplitScreen4:Clear()
+  self.screen1.clear()
+  self.screen2.clear()
+  self.screen3.clear()
+  self.screen4.clear()
+end
+
+function SplitScreen4:Display(svg)
+  self.screen1.addContent(0,0, self.viewbox1..svg..'</g></svg>')
+  self.screen2.addContent(0,0, self.viewbox2..svg..'</g></svg>')
+  self.screen3.addContent(0,0, self.viewbox3..svg..'</g></svg>')
+  self.screen4.addContent(0,0, self.viewbox4..svg..'</g></svg>')   
+end
+--
+ss4 = SplitScreen4:new{screen1, screen2, screen3, screen4, screenX = 306, screenY = 153, scale = 2}
+
+-- some svg stuff
+svgBackground ='<rect width="100vw" height="100vh" style="fill:rgb(0,0,255);stroke-width:10;stroke:rgb(255,255,0)" />'
+picture = svgBackground..logo
+
+function htmlBall(x,y,ballRayon)
+  local str = [[
+<svg width="1025" height="606">
+<defs> 
+   <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%"> 
+   <stop offset="0%" style="stop-color:rgb(200,200,255);stop-opacity:1" />  
+   <stop offset="100%" style="stop-color:rgb(0,15,15);stop-opacity:1" />  
+   </linearGradient>  
+</defs>  
+<circle cx="]]..x..[[" cy="]]..y..[[" r="]]..ballRayon..[[" stroke="black" stroke-width=".5" fill="url(#grad1)" />  
+</svg>]]
+
+ return str  
+end
+
+ss4:Display(picture)
 
 
--- viewbox1 = '<svg width="100vw" height="100vh" viewBox="0 0 100vw 100vh">'
-div = '<div style="width:100px;height:100px;display:inline-block;border:solid 1px green;">'
-viewbox2 = '<svg width="306" height="153" viewBox="0 0 306 153"><g width="306" height="153">'
-
-
-screen1.addContent(0,0, div..logo..'</div>')
-screen2.addContent(0,0,viewbox2..logo..'</g></svg>')
--- screen3.addContent(0,0, string.format(img, logo))
--- screen4.addContent(0,0, viewbox4..string.format(svgBackground, 'Screen4')..'</svg>')
-
--- background = screen1.addContent(0,0, '<div style="font-family: Arial; color: #FFFFFF; font-size:15; padding-top: 7vw; padding-left: 7vw;">screen1</div>')
--- background = screen2.addContent(0,0, 'screen2')
--- background = screen3.addContent(0,0, 'screen3')
--- background = screen4.addContent(0,0, 'screen4')
+-- timer
+self.setTimer('loop', .23)
